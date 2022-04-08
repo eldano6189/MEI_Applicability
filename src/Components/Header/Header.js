5ad54355aea9b3e4bc290111eebf1adbd614f0aa
eldano6189 <eldano6189@gmail.com>
2022-04-08 19:47:14 +0100

    added svg logo in colours of MI

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,6 +4,7 @@ import { useContext } from "react";
 import { useLocation, Link } from "react-router-dom";
 
 import Data from "../../Context/Context";
+import { REMEMI } from "../../Assets/SVG/REMEMI";
 import { SignOut } from "../Sign-out/SignOut";
 
 export const Header = () => {
@@ -11,31 +12,30 @@ export const Header = () => {
   const { loggedIn, user } = useContext(Data);
 
   return (
-    <header>
-      <div className={styles.containerLogo}>
-        <Link to="/">
-          <div className={styles.logo}>
-            <img
-              src={require("../../Assets/Images/REME.png")}
-              alt="Cap Badge"
-            />
-          </div>
-        </Link>
-        <h4 className="header">1MI Bn, Inspection Applicability</h4>
-      </div>
-      <div className={styles.containerSearch}>
-        {location.pathname === "/" ||
-        location.pathname === "/login" ||
-        location.pathname === "/changepassword" ? null : (
+    loggedIn && (
+      <header>
+        <div className={styles.containerLogo}>
           <Link to="/">
-            <button className="button">Back</button>
+            <div className={styles.logo}>
+              <REMEMI />
+            </div>
           </Link>
-        )}
-        {loggedIn ? (
-          <p className="paragraph">Logged in as : {user.username}</p>
-        ) : null}
-        {loggedIn ? <SignOut /> : null}
-      </div>
-    </header>
+          <h4 className="header">1MI Bn, Inspection Applicability</h4>
+        </div>
+        <div className={styles.containerInfo}>
+          {location.pathname === "/" ||
+          location.pathname === "/login" ||
+          location.pathname === "/changepassword" ? null : (
+            <Link to="/">
+              <button className="button">Back</button>
+            </Link>
+          )}
+          <p className="paragraph">
+            Logged in as : <span className="sub-header">{user.username}</span>
+          </p>
+          <SignOut />
+        </div>
+      </header>
+    )
   );
 };
--- src/Pages/Login-page/LoginPage.js
+++ src/Pages/Login-page/LoginPage.js
@@ -1,5 +1,6 @@
 import styles from "./LoginPage.module.css";
 
+import { REMEMI } from "../../Assets/SVG/REMEMI";
 import { Login } from "../../Components/Login/Login";
 
 export const LoginPage = () => {
@@ -7,7 +8,7 @@ export const LoginPage = () => {
     <div className={styles.container}>
       <h1 className="header">Welcome to the 1MI Applicability Register</h1>
       <div className={styles.logo}>
-        <img src={require("../../Assets/Images/REME.png")} alt="Cap Badge" />
+        <REMEMI />
       </div>
       <Login />
     </div>
